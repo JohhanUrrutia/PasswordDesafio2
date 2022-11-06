c1a6648659c94c1f54440df9e7e54cbe8d5e132c
Johhan Urrutia <johhanurrutia@MacBook-Pro-de-Johhan.local>
2022-11-06 17:58:32 -0300

    Seleccion Stickers v1.0

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+var inputUno = document.getElementById("inputUno")
+var inputDos = document.getElementById("inputDos")
+var inputTres = document.getElementById("inputTres")
+var botonIngresar = document.getElementById("botonIngresar")
+var validacionFinal = document.getElementById("validacionFinal")
+
+botonIngresar.addEventListener("click", ()=>{
+    if (inputUno.value == 9 && inputDos.value == 1 && inputTres.value == 1){
+        validacionFinal.innerHTML = "Password 1 Correcto"
+    }else if (inputUno.value == 7 && inputDos.value == 1 && inputTres.value == 4){
+        validacionFinal.innerHTML = "Password 2 Correcto"
+    } else{
+        validacionFinal.innerHTML = "Password Incorrecto"
+        validacionFinal.style.color = "red";
+    }
+})
