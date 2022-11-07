e0f8b476429842127b32d789fc51af82d7ecb36b
Johhan Urrutia <johhanurrutia@MacBook-Pro-de-Johhan.local>
2022-11-06 22:08:46 -0300

    Correccion Select v1.2

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,11 +7,12 @@ var validacionFinal = document.getElementById("validacionFinal")
 botonIngresar.addEventListener("click", ()=>{
     if (inputUno.value == 9 && inputDos.value == 1 && inputTres.value == 1){
         validacionFinal.innerHTML = "Password 1 Correcto"
+        validacionFinal.style.color = "green";
     }else if (inputUno.value == 7 && inputDos.value == 1 && inputTres.value == 4){
         validacionFinal.innerHTML = "Password 2 Correcto"
+        validacionFinal.style.color = "green";
     } else{
         validacionFinal.innerHTML = "Password Incorrecto"
         validacionFinal.style.color = "red";
-        setTimeout(function(){ location. reload() }, 2000);
     }
 })
